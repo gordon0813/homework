aa8aac83b23504e8bf29dd38f153188263959214
gordon0813 <gordon080849@gmail.com>
2018-12-05 18:01:07 +0800

    p

--- octave/HW4/B1.m
+++ octave/HW4/B1.m
@@ -8,8 +8,14 @@ endfunction
 function f1=f(x);
   f1=sin(3*x)+3*cos(6*x);
 endfunction
+function f1=p(x);
+  f1=x*6-x.^2;
+endfunction
 function f1=realdf(x);
   f1=3*cos(3*x)-18*sin(6*x);
+endfunction
+function f1=realdf2(x);
+  f1=6-2*x;
 endfunction
 function re=remove_boundry(x)
   L=size(x)(2);
@@ -40,12 +46,42 @@ yf32=real(ifft((y32.*[[0:1:15],[0],[-15:1:-1]])*1i));
 yreal=realdf(realx);
 
 figure(1)
+
 plot(x16,yc16,"r-*")
+title("n=16 b1")
 hold 
 plot(realx,yreal,"g")
 plot(fx16,yf16,"k-*")
 figure(2)
+
 plot(x32,yc32,"r-*")
+title("n=32 b1")
+hold
+plot(realx,yreal,"g")
+plot(fx32,yf32,"k-*")
+
+
+yc16=central(p(n16),n16(1,2)-n16(1,1));
+yc32=central(p(n32),n32(1,2)-n32(1,1));
+fx16=remove_boundry_s(n16);
+fx32=remove_boundry_s(n32);
+y16=fft(p(fx16));
+y32=fft(p(fx32));
+yf16=real(ifft((y16.*[[0:1:7],[0],[-7:1:-1]])*1i));
+yf32=real(ifft((y32.*[[0:1:15],[0],[-15:1:-1]])*1i));
+yreal=realdf2(realx);
+
+figure(3)
+
+plot(x16,yc16,"r-*")
+title("n=16 b2")
+hold 
+plot(realx,yreal,"g")
+plot(fx16,yf16,"k-*")
+figure(4)
+
+plot(x32,yc32,"r-*")
+title("n=32 b2")
 hold
 plot(realx,yreal,"g")
 plot(fx32,yf32,"k-*")
