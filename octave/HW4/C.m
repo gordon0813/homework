4a7c7f123ab9ef39e7ee80bac2d32f03692928d1
gordon0813 <gordon080849@gmail.com>
2018-12-08 10:40:02 +0800

    p

--- octave/HW4/C.m
+++ octave/HW4/C.m
@@ -19,28 +19,30 @@ function re=get_real_func(x)
    for j= 2:32
      re=re+re1(1,j)*cos(k(1,j)*axisx)-im1(1,j)*sin(k(1,j)*axisx);
    endfor
-   re=re/32
+   re=re
 endfunction
 axisx=linspace(0,2*pi,1387);
 n32=linspace(0,2*pi,33);
 x32=remove_boundry_s(n32);
 yf=f(x32);
 yg=g(x32);
-func1=get_real_func(fft(yf.*yg));
-testt=fft(yf.*yg)
-fk=fft(yf);
-gk=fft(yg);
-indexf=[fk,fk,fk];
-indexg=[gk,gk,gk];
-hm=[]
+func1=get_real_func(fft(yf.*yg/32));
+testt=fft(yf.*yg/32)
+fk=fft(yf)/32;
+gk=fft(yg)/32;
+indexf=[fk,fk](1,16:47);
+indexg=[gk,gk](1,16:47);
+hmm=[]
 for i =1:32
   for j=1:32
-    hm(i,j)=indexf(1,j+32)*indexg(1,i-j+32);
+    if(i-j>0)
+      hmm(j,i)=indexf(1,j)*indexg(1,i-j);
+    endif
   endfor
 endfor
 
-hm=sum(hm);
-
+hm=sum(hmm);
+#hm=[hm(1,17:32),hm(1,1:16)]
 func2=get_real_func(hm)#1,[[17:32],[1:16]])
 
 
@@ -50,9 +52,9 @@ figure(1);
 plot(axisx,func1,"r")#C.1
 hold
 plot(axisx,real_func,"b")#C.3
-#plot(axisx,func2,"g")
+plot(axisx,func2,"g")
 plot(x32,f(x32).*g(x32),"*") #¸ê®ÆÂI
-#figure(2);
-#plot([1:1:32],abs(hm),"r")
-#figure(3)
-#plot([1:1:32],abs(testt),"b-*")
+figure(2);
+plot([1:1:32],imag(hm),"r-*")
+hold
+plot([1:1:32],imag(testt),"b-*")
---
+++ octave/HW4/D.m
@@ -0,0 +1,58 @@
+clear all
+clc
+
+function re=u(x)
+  re=4*(x.^2-x.^4).*exp(-x/2)
+endfunction
+function re=firstd(x)
+  re=4*(exp(-x/2).*(2*x-4*x.^3)-1/2*exp(-x/2).*(x.^2-x.^4))
+endfunction
+function re=secd(x)
+  re=-47*exp(-x/2).*x.^2   -exp(-x/2).*x.^4+16*exp(-x/2).*x.^3-8*exp(-x/2).*x+8*exp(-x/2)
+endfunction
+d=[]
+n=12
+x=[0:1:n-1]
+x=cos(x/(n-1)*pi)
+#x=[1,0.809,0.309,-0.309,-0.809,-1]
+N=n-1
+cj=1
+ck=1
+for j =1:n
+  cj=1
+  if(j==1 || j==n)
+    cj=2
+  endif
+  for k =1:n
+    ck=1
+    
+    if(k==1 || k==n)
+      ck=2
+    endif
+    if(j!=k)
+      d(j,k)=cj*(-1)^(j+k-2)/(ck*(x(1,j)-x(1,k)))
+    elseif (j==k && j==1)
+      d(j,k)=(2*N^2+1)/6
+    elseif (j==k && j==n)
+      d(j,k)=-(2*N^2+1)/6
+    else
+      d(j,k)=-x(1,j)/(2*(1-x(1,j)^2))
+    endif
+  end
+end
+u1=u(x)
+t1=rot90(u1,-1)
+yd=d*rot90(u1,-1)
+figure(1)
+plot(x,yd,"*")
+xreal=linspace(-1,1,33)
+fd=firstd(xreal)
+
+hold
+plot(xreal,fd,"g")
+
+ydd=d*(d*t1)
+figure(2)
+plot(x,ydd,"*")
+hold
+plot(xreal,secd(xreal))
